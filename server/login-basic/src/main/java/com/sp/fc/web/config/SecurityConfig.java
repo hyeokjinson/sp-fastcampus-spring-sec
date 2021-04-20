0721113de7703c790bf5c1185c5ba355e17f7fc4
hyeokjinson <hjson817@gmail.com>
2021-04-20 18:26:27 +0900

    04.20 commit Spring Security

--- server/login-basic/src/main/java/com/sp/fc/web/config/SecurityConfig.java
+++ server/login-basic/src/main/java/com/sp/fc/web/config/SecurityConfig.java
@@ -1,21 +1,75 @@
 package com.sp.fc.web.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 
 @EnableWebSecurity(debug = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final CustomAutoDetails customAutoDetails;
+    public SecurityConfig(CustomAutoDetails customAutoDetails){
+        this.customAutoDetails=customAutoDetails;
+    }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser(
+                        User.withDefaultPasswordEncoder()
+                                .username("user1")
+                                .password("1111")
+                                .roles("USER")
+                ).withUser(
+                User.withDefaultPasswordEncoder()
+                        .username("admin")
+                        .password("2222")
+                        .roles("ADMIN")
+        );
 
+    }
+
+    @Bean
+    RoleHierarchy roleHierarchy(){
+        RoleHierarchyImpl roleHierarchy=new RoleHierarchyImpl();
+        roleHierarchy.setHierarchy("ROLE_ADIMIN>ROLE_USER");
+        return roleHierarchy;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests(request->{
                     request
-                            .antMatchers("/**").permitAll()
-                            ;
+                            .antMatchers("/").permitAll()
+                            .anyRequest().authenticated();
+
                 })
+                .formLogin(
+                        login->login.loginPage("/login")
+                        .permitAll()
+                        .defaultSuccessUrl("/",false)
+                        .failureUrl("/login-error")
+                        .authenticationDetailsSource(customAutoDetails)
+                )
+                .logout(logout->logout.logoutSuccessUrl("/"))
+                .exceptionHandling(exception->exception.accessDeniedPage("/access-denied"))
                 ;
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .requestMatchers(
+                        PathRequest.toStaticResources().atCommonLocations()
+                );
     }
 }
